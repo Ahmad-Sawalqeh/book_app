361932338afb703dd5e9c45895312211c6db2dcb
ahmad-sawalqeh <sawalqa_jo@hotmail.com>
2019-11-25 13:47:22 +0200

    post for searches

--- server.js
+++ server.js
@@ -2,13 +2,13 @@
 
 require('dotenv').config();
 const express = require('express');
-// const superagent = require('superagent');
+const superagent = require('superagent');
 
 const PORT = process.env.PORT || 3000;
 const app = express();
 
 app.use(express.static('./public'));
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({extended:true,}));
 
 app.set('view engine', 'ejs');
 
@@ -16,9 +16,35 @@ app.get('/', (req,res) => {
   res.render('pages/index');
 });
 
-app.get('/searches', (req,res) => {
-  res.render('pages/searches/show');
+app.post('/searches', (req,res) => {
+  let url = searchUrl(req);
+  superagent.get(url)
+    .then( data => {
+      console.log('data : ', data);
+      res.render('pages/searches/show');
+      // , {searches:data.body.items,}
+    });
 });
 
-
 app.listen(PORT, () => console.log('Up on port', PORT));
+
+
+
+
+
+
+
+
+
+
+
+
+function searchUrl(request) {
+  let url = 'https://www.googleapis.com/books/v1/volumes?q=';
+  if (request.body.search[1] === 'title') {
+    url += `${request.body.search[0]}+intitle`;
+  }else if (request.body.search[1] === 'author') {
+    url += `${request.body.search[0]}+inauthor`;
+  }
+  return url;
+}
