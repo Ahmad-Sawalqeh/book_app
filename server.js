f26b0fbee89138bfd78e4f351f3a8938144bfbe0
ahmad-sawalqeh <sawalqa_jo@hotmail.com>
2020-01-12 02:49:19 +0200

    full application

--- server.js
+++ server.js
@@ -15,15 +15,6 @@ app.use(express.urlencoded({ extended: true, }));
 app.set('view engine', 'ejs');
 app.use(methodOverride(putDeleteMethods));
 
-app.use(methodOverride((req, res) => {
-  if (req.body && typeof req.body === 'object' && '_method' in req.body) {
-    // look in urlencoded POST bodies and delete it
-    let method = req.body._method;
-    delete req.body._method;
-    return method;
-  }
-}));
-
 app.get('/', getFromDataBase);
 app.get('/search', getSearchForm);
 app.post('/search/result', getApiBooks);
